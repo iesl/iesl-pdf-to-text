6734598cf7d661e5568445b787bd98776944b603
Thomas Logan <thomaslilogan@gmail.com>
2014-12-03 10:58:34 -0500

    changed js to combine svg pages under one root and write to one file

--- src/main/js/pdfToSvg.js
+++ src/main/js/pdfToSvg.js
@@ -7,19 +7,22 @@ define(function(require) {
     var fs = require('fs');
 
     // Dumps svg outputs to a folder called svgdump
-    function writeToFile(svgdump, pageNum, pdfPath) {
+    function writeToFile(svgdump, pdfPath) {
         var name = getFileNameFromPath(pdfPath);
-        var dirName = name + ".d"
+        var dirName = "svgdump";
         fs.mkdir('./' + dirName + '/', function(err) {
             if (!err || err.code === 'EEXIST') {
-                fs.writeFile('./' + dirName + '/' + name + "-" + pageNum + '.svg', svgdump,
-                             function(err) {
-                                 if (err) {
-                                     console.log('Error: ' + err);
-                                 } else {
-                                     console.log('Page: ' + pageNum);
-                                 }
-                             });
+              fs.writeFile(
+                './' + dirName + '/' + name + '.svg', 
+                svgdump,
+                function(err) {
+                    if (err) {
+                        console.log('Error: ' + err);
+                    } else {
+                        console.log('Name: ' + name);
+                    }
+                }
+              );
             }
         });
     }
@@ -67,40 +70,107 @@ define(function(require) {
             require('./node/domstubs.js');
 
 
+            var _ = require('underscore');
+
             PDFJS.getDocument(data).then(function (doc) {
-                var numPages = doc.numPages;
-                console.log('# Document Loaded');
-                console.log('Number of Pages: ' + numPages);
-                console.log();
+              var numPages = doc.numPages;
+              var svgBookPromise = _.foldl(_.range(1, numPages + 1), function(svgBookAccPro, pageNum) {
 
-                var lastPromise = Promise.resolve(); // will be used to chain promises
-                var loadPage = function (pageNum) {
-                    return doc.getPage(pageNum).then(function (page) {
-                        console.log('# Page ' + pageNum);
-                        var viewport = page.getViewport(1.0 /* scale */);
-                        console.log('Size: ' + viewport.width + 'x' + viewport.height);
-                        console.log();
+                return Promise.resolve(svgBookAccPro.then(function(svgBookAcc) {
 
-                        return page.getOperatorList().then(function (opList) {
-                            var svgGfx = new PDFJS.SVGGraphics(page.commonObjs, page.objs);
-                            svgGfx.embedFonts = true;
-                            return svgGfx.getSVG(opList, viewport).then(function (svg) {
-                                var svgDump = svg.toString();
-                                writeToFile(svgDump, pageNum, filename);
-                            });
+                  return Promise.resolve(doc.getPage(pageNum).then(function(page) {
+                    var viewport = page.getViewport(1.0 /* scale */);
+
+                    return Promise.resolve(page.getOperatorList().then(function (opList) {
+                        var svgGfx = new PDFJS.SVGGraphics(page.commonObjs, page.objs);
+                        svgGfx.embedFonts = true;
+
+                        return svgGfx.getSVG(opList, viewport).then(function(svgPage) {
+                          svgBookAcc[pageNum] = svgPage;
+                          return svgBookAcc;
                         });
-                    });
+
+                    }));
+
+                  }));
+
+                }));
+
+
+              }, Promise.resolve({}));
+
+
+              svgBookPromise.then(function(svgBook) {
+                for (key in svgBook) {
+                  var svg = svgBook[key]
+                  var svgBody = svg.childNodes[0]
+                }
+
+
+                var getYTransformFromSvgMatrix = function(svgMatrix) {
+
+                  var arr = svgMatrix.split(" ");
+                  if (svgMatrix.slice(0, 7) === "matrix(" && arr.length === 6) {
+                    return parseFloat(arr[5].slice(0, arr.length - 1));
+                  } else {
+                    throw new Error('String has incorrect format: '+ svgMatrix +'. should be: matrix(0 0 0 0 0 0)');
+                  } 
+
                 };
 
-                for (var i = 1; i <= numPages; i++) {
-                    lastPromise = lastPromise.then(loadPage.bind(null, i));
+                var findHeight = function(svgE) {
+                  return parseFloat(svgE.attributes.height.toString().slice(0, -2));
+                };
+
+                var findViewBoxY = function(svgE) {
+                  return parseFloat(svgE.attributes.viewBox.toString().split(" ")[3]);
                 }
-                return lastPromise;
+
+                var svgPages = _.values(svgBook);
+                var combinedSvg = _.foldl(_.tail(svgPages), function(combinedSvgAcc, svgPage) {
+
+                  var totalHeight = findHeight(combinedSvgAcc) + findHeight(svgPage);
+                  var totalViewBoxY = findViewBoxY(combinedSvgAcc) + findViewBoxY(svgPage);
+                  var numChildren = combinedSvgAcc.childNodes.length
+                  var accTransform = combinedSvgAcc.childNodes[numChildren - 1].attributes.transform.toString();
+
+                  var svgPageChild = svgPage.childNodes[0];
+                  var svgPageTransform = svgPageChild.attributes.transform.toString();
+
+                  var transformOffset = getYTransformFromSvgMatrix(accTransform) + getYTransformFromSvgMatrix(svgPageTransform);
+
+                  var _svgPageChildTransform = (function() {
+                    var arr = svgPageTransform.split(" ");
+                    arr[5] = transformOffset + ")";
+                    return arr.join(" ");
+                  })();
+
+                  svgPageChild.setAttributeNS(null, 'transform', _svgPageChildTransform); 
+
+                  combinedSvgAcc.setAttributeNS(null, 'height', totalHeight + "px");
+                  var _viewBox = (function() {
+                    var arr = combinedSvgAcc.attributes.viewBox.toString().split(" ");
+                    arr[3] = totalViewBoxY;
+                    return arr.join(" ");
+                  })();
+                  combinedSvgAcc.setAttributeNS(null, 'viewBox', _viewBox);
+                  combinedSvgAcc.appendChild(svgPageChild);
+
+                  return combinedSvgAcc;
+
+                }, _.head(svgPages));
+
+                 
+                writeToFile(combinedSvg.toString(), filename);
+                
+              });
+            
             }).then(function () {
                 console.log('# End of Document');
             }, function (err) {
                 console.error('Error: ' + err);
             });
+
         });
 
     }
