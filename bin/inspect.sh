7d55b51507d49450380a59c34f3a50f3c254e042
Thomas Logan <thomaslilogan@gmail.com>
2015-01-29 13:42:00 -0500

    added txt output arg to inspector and created convenience script to run it

---
+++ bin/inspect.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sbt "runMain edu.umass.cs.iesl.iesl_pdf_to_text.Inspector $1 $2 $3"
--- src/main/scala/Inspector.scala
+++ src/main/scala/Inspector.scala
@@ -1,17 +1,19 @@
 package edu.umass.cs.iesl.iesl_pdf_to_text
 
 import java.io.PrintWriter
+import java.io.File
 
 object Inspector {
   def main(args: Array[String]): Unit = {
 
     val input = args(0)
-    val output = args(1)
+    val outputSvg = args(1)
+    val outputTxt = args(2)
 
-    runPdfToSvg(input, output)
-    val tspanSeq = getTspanSeq(mkDom(output))
+    runPdfToSvg(input, outputSvg)
+    val tspanSeq = getTspanSeq(mkDom(outputSvg))
 
-    val writer = new PrintWriter(output)
+    val writer = new PrintWriter(new File(outputTxt))
     tspanSeq.foreach(tspan => {
       writer.println(tspan.getText())
       writer.println()
